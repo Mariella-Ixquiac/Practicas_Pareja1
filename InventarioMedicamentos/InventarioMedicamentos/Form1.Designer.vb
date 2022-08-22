d319c7a54985a5a5f25143b2d2055a96193a2748
YulianaSoto <104378187+YulianaSoto@users.noreply.github.com>
2022-08-22 09:21:51 -0600

    EJEMPLO DE CAMBIO

--- InventarioMedicamentos/InventarioMedicamentos/Form1.Designer.vb
+++ InventarioMedicamentos/InventarioMedicamentos/Form1.Designer.vb
@@ -32,18 +32,20 @@ Partial Class Form1
         '
         'Button1
         '
-        Me.Button1.Location = New System.Drawing.Point(69, 93)
+        Me.Button1.Location = New System.Drawing.Point(92, 114)
+        Me.Button1.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(115, 23)
+        Me.Button1.Size = New System.Drawing.Size(153, 28)
         Me.Button1.TabIndex = 0
         Me.Button1.Text = "Ingresar"
         Me.Button1.UseVisualStyleBackColor = True
         '
         'Button2
         '
-        Me.Button2.Location = New System.Drawing.Point(190, 93)
+        Me.Button2.Location = New System.Drawing.Point(253, 114)
+        Me.Button2.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.Button2.Name = "Button2"
-        Me.Button2.Size = New System.Drawing.Size(115, 23)
+        Me.Button2.Size = New System.Drawing.Size(153, 28)
         Me.Button2.TabIndex = 1
         Me.Button2.Text = "Salir"
         Me.Button2.UseVisualStyleBackColor = True
@@ -51,40 +53,44 @@ Partial Class Form1
         'Label1
         '
         Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(30, 24)
+        Me.Label1.Location = New System.Drawing.Point(40, 30)
+        Me.Label1.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(43, 13)
+        Me.Label1.Size = New System.Drawing.Size(57, 17)
         Me.Label1.TabIndex = 2
         Me.Label1.Text = "Usuario"
         '
         'Label2
         '
         Me.Label2.AutoSize = True
-        Me.Label2.Location = New System.Drawing.Point(22, 56)
+        Me.Label2.Location = New System.Drawing.Point(29, 69)
+        Me.Label2.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
         Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(61, 13)
+        Me.Label2.Size = New System.Drawing.Size(81, 17)
         Me.Label2.TabIndex = 3
         Me.Label2.Text = "Contraseña"
         '
         'TextBox1
         '
-        Me.TextBox1.Location = New System.Drawing.Point(89, 21)
+        Me.TextBox1.Location = New System.Drawing.Point(119, 26)
+        Me.TextBox1.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.TextBox1.Name = "TextBox1"
-        Me.TextBox1.Size = New System.Drawing.Size(253, 20)
+        Me.TextBox1.Size = New System.Drawing.Size(336, 22)
         Me.TextBox1.TabIndex = 4
         '
         'TextBox2
         '
-        Me.TextBox2.Location = New System.Drawing.Point(89, 53)
+        Me.TextBox2.Location = New System.Drawing.Point(119, 65)
+        Me.TextBox2.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.TextBox2.Name = "TextBox2"
-        Me.TextBox2.Size = New System.Drawing.Size(253, 20)
+        Me.TextBox2.Size = New System.Drawing.Size(336, 22)
         Me.TextBox2.TabIndex = 5
         '
         'Form1
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(371, 145)
+        Me.ClientSize = New System.Drawing.Size(495, 178)
         Me.ControlBox = False
         Me.Controls.Add(Me.TextBox2)
         Me.Controls.Add(Me.TextBox1)
@@ -92,6 +98,7 @@ Partial Class Form1
         Me.Controls.Add(Me.Label1)
         Me.Controls.Add(Me.Button2)
         Me.Controls.Add(Me.Button1)
+        Me.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.Name = "Form1"
         Me.Text = "Login"
         Me.ResumeLayout(False)
