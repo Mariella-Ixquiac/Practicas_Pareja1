4d5a54a68168444e1025a78abef51a18590c1dd7
YulianaSoto <104378187+YulianaSoto@users.noreply.github.com>
2022-08-26 12:04:38 -0600

    Log in

--- InventarioMedicamentos/InventarioMedicamentos/Form1.vb
+++ InventarioMedicamentos/InventarioMedicamentos/Form1.vb
@@ -1,5 +1,17 @@
-﻿Public Class Form1
+﻿Imports System.Text
+Imports System.Security.Cryptography
+
+Public Class Form1
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim codigo As New UnicodeEncoding()
+        Dim md5 As New MD5CryptoServiceProvider()
+        Dim clave As String
+        clave = TextBox2.Text
+        Dim hash() As Byte = md5.ComputeHash(codigo.GetBytes(clave))
+        Dim nuevaclave As String
+        nuevaclave = Convert.ToBase64String(hash)
+
+
         Index.Show()
         Me.Hide()
     End Sub
