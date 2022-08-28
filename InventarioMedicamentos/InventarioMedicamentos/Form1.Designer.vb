498d959d2518558971fc08eb9a053bf8ebe366f3
Mariella-Ixquiac <imixquiachernandez2005@elvallecolegio.edu.gt>
2022-08-27 19:52:43 -0600

    LogIn-intento

--- InventarioMedicamentos/InventarioMedicamentos/Form1.Designer.vb
+++ InventarioMedicamentos/InventarioMedicamentos/Form1.Designer.vb
@@ -32,16 +32,17 @@ Partial Class Form1
         Me.Label3 = New System.Windows.Forms.Label()
         Me.Label4 = New System.Windows.Forms.Label()
         Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.CheckBox1 = New System.Windows.Forms.CheckBox()
         CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'Button1
         '
         Me.Button1.BackColor = System.Drawing.Color.Turquoise
-        Me.Button1.Location = New System.Drawing.Point(16, 102)
+        Me.Button1.Location = New System.Drawing.Point(31, 124)
         Me.Button1.Margin = New System.Windows.Forms.Padding(4)
         Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(185, 26)
+        Me.Button1.Size = New System.Drawing.Size(155, 26)
         Me.Button1.TabIndex = 0
         Me.Button1.Text = "Ingresar"
         Me.Button1.UseVisualStyleBackColor = False
@@ -49,10 +50,10 @@ Partial Class Form1
         'Button2
         '
         Me.Button2.BackColor = System.Drawing.Color.Turquoise
-        Me.Button2.Location = New System.Drawing.Point(209, 102)
+        Me.Button2.Location = New System.Drawing.Point(193, 124)
         Me.Button2.Margin = New System.Windows.Forms.Padding(4)
         Me.Button2.Name = "Button2"
-        Me.Button2.Size = New System.Drawing.Size(177, 26)
+        Me.Button2.Size = New System.Drawing.Size(147, 26)
         Me.Button2.TabIndex = 1
         Me.Button2.Text = "Salir"
         Me.Button2.UseVisualStyleBackColor = False
@@ -63,7 +64,7 @@ Partial Class Form1
         Me.Label1.Location = New System.Drawing.Point(13, 48)
         Me.Label1.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(54, 16)
+        Me.Label1.Size = New System.Drawing.Size(45, 14)
         Me.Label1.TabIndex = 2
         Me.Label1.Text = "Usuario:"
         '
@@ -73,7 +74,7 @@ Partial Class Form1
         Me.Label2.Location = New System.Drawing.Point(13, 75)
         Me.Label2.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
         Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(74, 16)
+        Me.Label2.Size = New System.Drawing.Size(62, 14)
         Me.Label2.TabIndex = 3
         Me.Label2.Text = "Contraseña:"
         '
@@ -82,16 +83,16 @@ Partial Class Form1
         Me.TextBox1.Location = New System.Drawing.Point(75, 42)
         Me.TextBox1.Margin = New System.Windows.Forms.Padding(4)
         Me.TextBox1.Name = "TextBox1"
-        Me.TextBox1.Size = New System.Drawing.Size(311, 22)
+        Me.TextBox1.Size = New System.Drawing.Size(266, 19)
         Me.TextBox1.TabIndex = 4
         '
         'TextBox2
         '
-        Me.TextBox2.Location = New System.Drawing.Point(95, 72)
+        Me.TextBox2.Location = New System.Drawing.Point(83, 72)
         Me.TextBox2.Margin = New System.Windows.Forms.Padding(4)
         Me.TextBox2.Name = "TextBox2"
         Me.TextBox2.PasswordChar = Global.Microsoft.VisualBasic.ChrW(42)
-        Me.TextBox2.Size = New System.Drawing.Size(291, 22)
+        Me.TextBox2.Size = New System.Drawing.Size(257, 19)
         Me.TextBox2.TabIndex = 5
         '
         'Label3
@@ -101,7 +102,7 @@ Partial Class Form1
         Me.Label3.Location = New System.Drawing.Point(0, 0)
         Me.Label3.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
         Me.Label3.Name = "Label3"
-        Me.Label3.Size = New System.Drawing.Size(660, 28)
+        Me.Label3.Size = New System.Drawing.Size(636, 28)
         Me.Label3.TabIndex = 50
         Me.Label3.TextAlign = System.Drawing.ContentAlignment.BottomCenter
         '
@@ -111,7 +112,7 @@ Partial Class Form1
         Me.Label4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
         Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.Label4.ForeColor = System.Drawing.Color.White
-        Me.Label4.Location = New System.Drawing.Point(-12, 139)
+        Me.Label4.Location = New System.Drawing.Point(-13, 159)
         Me.Label4.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
         Me.Label4.Name = "Label4"
         Me.Label4.Padding = New System.Windows.Forms.Padding(20, 0, 0, 0)
@@ -122,20 +123,31 @@ Partial Class Form1
         'PictureBox1
         '
         Me.PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), System.Drawing.Image)
-        Me.PictureBox1.Location = New System.Drawing.Point(393, 42)
+        Me.PictureBox1.Location = New System.Drawing.Point(368, 48)
         Me.PictureBox1.Name = "PictureBox1"
         Me.PictureBox1.Size = New System.Drawing.Size(256, 86)
         Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
         Me.PictureBox1.TabIndex = 134
         Me.PictureBox1.TabStop = False
         '
+        'CheckBox1
+        '
+        Me.CheckBox1.AutoSize = True
+        Me.CheckBox1.Location = New System.Drawing.Point(222, 98)
+        Me.CheckBox1.Name = "CheckBox1"
+        Me.CheckBox1.Size = New System.Drawing.Size(118, 18)
+        Me.CheckBox1.TabIndex = 135
+        Me.CheckBox1.Text = "Mostrar Contraseña"
+        Me.CheckBox1.UseVisualStyleBackColor = True
+        '
         'Form1
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.BackColor = System.Drawing.Color.Honeydew
-        Me.ClientSize = New System.Drawing.Size(660, 165)
+        Me.ClientSize = New System.Drawing.Size(636, 186)
         Me.ControlBox = False
+        Me.Controls.Add(Me.CheckBox1)
         Me.Controls.Add(Me.PictureBox1)
         Me.Controls.Add(Me.Label4)
         Me.Controls.Add(Me.Label3)
@@ -165,4 +177,5 @@ Partial Class Form1
     Private WithEvents Label3 As Label
     Private WithEvents Label4 As Label
     Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents CheckBox1 As CheckBox
 End Class
--- InventarioMedicamentos/InventarioMedicamentos/Form1.vb
+++ InventarioMedicamentos/InventarioMedicamentos/Form1.vb
@@ -1,22 +1,68 @@
 ﻿Imports System.Text
 Imports System.Security.Cryptography
 
+Imports MySql.Data.MySqlClient
+Imports MySql.Data
+Imports System.Configuration
+
 Public Class Form1
+    Dim CadenaConexion = "Server = localhost;Database=inventario_medicamentos;User id=root;Password=;Port=3306;"
+    Dim conn As New MySqlConnection(CadenaConexion)
+    Dim cmd As MySqlCommand
+
+    Private Sub conectar()
+        Dim squery As String = "SELECT * FROM login"
+        Dim adpt As New MySqlDataAdapter(squery, conn)
+        Dim ds As New DataSet()
+        adpt.Fill(ds)
+        conn.Close()
+        conn.Dispose()
+
+    End Sub
+
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If TextBox1.Text.Length = 0 Then
+            MsgBox("Debe Ingresar un Usuario")
+            TextBox1.Focus()
+            Exit Sub
+        End If
+        If TextBox2.Text.Length = 0 Then
+            MsgBox("Debe Ingresar una Contraseña")
+            TextBox2.Focus()
+            Exit Sub
+        End If
+
         Dim codigo As New UnicodeEncoding()
-        Dim md5 As New MD5CryptoServiceProvider()
-        Dim clave As String
-        clave = TextBox2.Text
-        Dim hash() As Byte = md5.ComputeHash(codigo.GetBytes(clave))
-        Dim nuevaclave As String
+            Dim md5 As New MD5CryptoServiceProvider()
+            Dim clave As String
+            clave = TextBox2.Text
+            Dim hash() As Byte = md5.ComputeHash(codigo.GetBytes(clave))
+            Dim nuevaclave As String
         nuevaclave = Convert.ToBase64String(hash)
 
 
+
         Index.Show()
-        Me.Hide()
+                Me.Hide()
     End Sub
 
     Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
         Me.Close()
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        If conn.State = ConnectionState.Closed Then
+            conn.Open()
+        End If
+        Call conectar()
+    End Sub
+
+    Private Sub CheckBox1_CheckedChanged(sender As Object, e As EventArgs) Handles CheckBox1.CheckedChanged
+        If CheckBox1.Checked Then
+            TextBox2.PasswordChar = ""
+        Else
+            TextBox2.PasswordChar = "*"
+        End If
+
     End Sub
 End Class
