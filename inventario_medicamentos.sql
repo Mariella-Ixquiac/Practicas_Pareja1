1f746c21f96fdfc04d98eef937fc89382142f64c
YulianaSoto <104378187+YulianaSoto@users.noreply.github.com>
2022-08-30 10:15:41 -0600

    Base_de_datos

---
+++ inventario_medicamentos.sql
@@ -0,0 +1,195 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1:3306
+-- Tiempo de generación: 30-08-2022 a las 16:11:56
+-- Versión del servidor: 5.7.36
+-- Versión de PHP: 7.4.26
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `inventario_medicamentos`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `clientes`
+--
+
+DROP TABLE IF EXISTS `clientes`;
+CREATE TABLE IF NOT EXISTS `clientes` (
+  `id_cliente` int(11) NOT NULL AUTO_INCREMENT,
+  `nom_cliente` varchar(100) NOT NULL,
+  `ape_cliente` varchar(100) NOT NULL,
+  `nit` varchar(13) NOT NULL,
+  `telefono_cliente` varchar(8) NOT NULL,
+  PRIMARY KEY (`id_cliente`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `compra`
+--
+
+DROP TABLE IF EXISTS `compra`;
+CREATE TABLE IF NOT EXISTS `compra` (
+  `id_compra` int(11) NOT NULL AUTO_INCREMENT,
+  `fec_compra` date NOT NULL,
+  `precio_costo` varchar(10) NOT NULL,
+  `unidades_compradas` int(11) NOT NULL,
+  `id_proveedores` int(11) NOT NULL,
+  `id_medicamento` int(11) NOT NULL,
+  `total` double DEFAULT NULL,
+  `precio_final` double DEFAULT NULL,
+  PRIMARY KEY (`id_compra`),
+  KEY `proveedores` (`id_proveedores`),
+  KEY `index_med` (`id_medicamento`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `login`
+--
+
+DROP TABLE IF EXISTS `login`;
+CREATE TABLE IF NOT EXISTS `login` (
+  `id_login` int(11) NOT NULL AUTO_INCREMENT,
+  `usuario` varchar(50) NOT NULL,
+  `nombre` varchar(50) NOT NULL,
+  `rol` varchar(20) NOT NULL,
+  `pssw` varchar(30) NOT NULL,
+  PRIMARY KEY (`id_login`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `login`
+--
+
+INSERT INTO `login` (`id_login`, `usuario`, `nombre`, `rol`, `pssw`) VALUES
+(1, 'Admin1', 'Alejandro Mendez Mendez', 'Administrador', '12345');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `marca`
+--
+
+DROP TABLE IF EXISTS `marca`;
+CREATE TABLE IF NOT EXISTS `marca` (
+  `id_marca` int(11) NOT NULL AUTO_INCREMENT,
+  `nom_marca` varchar(100) NOT NULL,
+  `calificacion` varchar(1) NOT NULL,
+  `direccion` varchar(100) NOT NULL,
+  PRIMARY KEY (`id_marca`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `medicamento`
+--
+
+DROP TABLE IF EXISTS `medicamento`;
+CREATE TABLE IF NOT EXISTS `medicamento` (
+  `id_med` int(11) NOT NULL AUTO_INCREMENT,
+  `nom_med` varchar(100) NOT NULL,
+  `receta` tinyint(1) NOT NULL,
+  `precauciones` varchar(200) NOT NULL,
+  `descripcion` varchar(200) NOT NULL,
+  `fec_caducidad` date NOT NULL,
+  `presentacion` varchar(100) NOT NULL,
+  `uni_medida` varchar(100) NOT NULL,
+  `formula` varchar(200) NOT NULL,
+  `dosis` varchar(100) NOT NULL,
+  `id_venta` int(11) NOT NULL,
+  `id_compra` int(11) NOT NULL,
+  PRIMARY KEY (`id_med`),
+  KEY `compra_index` (`id_compra`),
+  KEY `venta_index` (`id_venta`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `proveedores`
+--
+
+DROP TABLE IF EXISTS `proveedores`;
+CREATE TABLE IF NOT EXISTS `proveedores` (
+  `id_proveedores` int(11) NOT NULL AUTO_INCREMENT,
+  `nom_proveedores` varchar(100) NOT NULL,
+  `telefono` varchar(8) NOT NULL,
+  `id_marca` int(11) NOT NULL,
+  PRIMARY KEY (`id_proveedores`),
+  KEY `Marca` (`id_marca`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `venta`
+--
+
+DROP TABLE IF EXISTS `venta`;
+CREATE TABLE IF NOT EXISTS `venta` (
+  `id_venta` int(11) NOT NULL AUTO_INCREMENT,
+  `fec_venta` date NOT NULL,
+  `unidades_vendidas` int(11) NOT NULL,
+  `subtotal_venta` double DEFAULT NULL,
+  `id_cliente` int(11) NOT NULL,
+  `id_medicamento` int(11) NOT NULL,
+  `id_compra` int(11) NOT NULL,
+  PRIMARY KEY (`id_venta`),
+  KEY `cliente_index` (`id_cliente`),
+  KEY `index_med` (`id_medicamento`),
+  KEY `index_compra` (`id_compra`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `compra`
+--
+ALTER TABLE `compra`
+  ADD CONSTRAINT `compra_ibfk_1` FOREIGN KEY (`id_compra`) REFERENCES `medicamento` (`id_compra`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `compra_ibfk_2` FOREIGN KEY (`id_proveedores`) REFERENCES `proveedores` (`id_proveedores`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `medicamento`
+--
+ALTER TABLE `medicamento`
+  ADD CONSTRAINT `medicamento_ibfk_1` FOREIGN KEY (`id_compra`) REFERENCES `compra` (`id_compra`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `medicamento_ibfk_2` FOREIGN KEY (`id_venta`) REFERENCES `venta` (`id_venta`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `proveedores`
+--
+ALTER TABLE `proveedores`
+  ADD CONSTRAINT `proveedores_ibfk_1` FOREIGN KEY (`id_marca`) REFERENCES `marca` (`id_marca`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `venta`
+--
+ALTER TABLE `venta`
+  ADD CONSTRAINT `venta_ibfk_1` FOREIGN KEY (`id_venta`) REFERENCES `medicamento` (`id_venta`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `venta_ibfk_2` FOREIGN KEY (`id_cliente`) REFERENCES `clientes` (`id_cliente`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
