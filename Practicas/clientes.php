96ee1af02466afc6d7f54ec8397366b2a4573236
Mariella-Ixquiac <imixquiachernandez2005@elvallecolegio.edu.gt>
2022-09-17 21:58:19 -0600

    Push
    Pagina

---
+++ Practicas/clientes.php
@@ -0,0 +1,96 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">	
+    <!-- CSS only -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+    <!-- JavaScript Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
+</head>
+<body bgcolor="#006316" style="font-family: 'Georgia MT';">
+
+<nav class="navbar" style="background-color: #008080;">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Inventario de Medicamento - Apoyo Técnico</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasDarkNavbar" aria-controls="offcanvasDarkNavbar">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="offcanvas offcanvas-end " style="background-color: #008080;" tabindex="-1" id="offcanvasDarkNavbar" aria-labelledby="offcanvasDarkNavbarLabel">
+      <div class="offcanvas-header">
+        <h3 class="offcanvas-title" id="offcanvasDarkNavbarLabel"> Menú</h3>
+        <button type="button" class="btn-close btn-close-white" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+      </div>
+      <div class="offcanvas-body">
+        <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="index.php">Inicio</a>
+          </li>
+          <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="inventario.php "> Inventario Médico</a>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="proveedores.php ">Proveedores y Marcas</a>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="clientes.php ">Clientes</a>
+                </li>
+        </ul>        
+      </div>
+    </div>
+  </div>
+</nav>
+
+
+        <div class="container-sm">
+        <?php
+        $user = "root";
+        $pass = "";
+        $host = "localhost";
+        $conneccion = mysqli_connect($host, $user, $pass);
+        if(!$conneccion) 
+                {
+                    echo "No se ha podido conectar con el servidor" . mysql_error();
+                }
+
+        $DB = "inventario_medicamentos";
+        $dbselect = mysqli_select_db($conneccion,$DB);
+        if (!$dbselect)
+        {
+        echo "No se ha podido encontrar la Base de Datos";
+        }
+
+        $consulta = "SELECT * FROM `clientes`
+
+        ";
+        $result = mysqli_query($conneccion,$consulta);
+        
+        if(!$result) 
+        {
+            echo "No se ha podido realizar la consulta";
+        }
+              
+        echo '<table class="table table-hover table-striped-columns">';
+        echo "<tr>";
+        echo '<th scope="col"><h4>ID</th></h4>';
+        echo '<th scope="col"><h4> Nombre del Cliente </th></h4>';
+        echo '<th scope="col"><h4> Apellido del Cliente </th></h4>';
+        echo '<th scope="col"><h4> Teléfono </th></h4>';
+        echo "</tr>";
+        while ($colum = mysqli_fetch_array($result))
+        {
+            echo "<tr>";
+            echo "<td>" . $colum['id_cliente'] . "</td>";
+            echo "<td>" . $colum['nom_cliente'] . "</td>";
+            echo "<td>" . $colum['ape_cliente'] . "</td>";
+            echo "<td>" . $colum['telefono_cliente'] . "</td>";
+            echo "</tr>";
+        }
+        echo "</table>"; ?>
+
+        
+</div>
+
+    </body>
+</html>
---
+++ Practicas/index.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+        <!-- JavaScript Bundle with Popper -->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
+</head>
+<body bgcolor="#006316" style="font-family: 'Georgia MT';">
+
+<nav class="navbar" style="background-color: #008080;">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Inventario de Medicamento - Apoyo Técnico</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasDarkNavbar" aria-controls="offcanvasDarkNavbar">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="offcanvas offcanvas-end " style="background-color: #008080;" tabindex="-1" id="offcanvasDarkNavbar" aria-labelledby="offcanvasDarkNavbarLabel">
+      <div class="offcanvas-header">
+        <h3 class="offcanvas-title" id="offcanvasDarkNavbarLabel"> Menú</h3>
+        <button type="button" class="btn-close btn-close-white" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+      </div>
+      <div class="offcanvas-body">
+        <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="index.php">Inicio</a>
+          </li>
+          <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="inventario.php "> Inventario Médico</a>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="proveedores.php ">Proveedores y Marcas</a>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="clientes.php ">Clientes</a>
+                </li>
+        </ul>        
+      </div>
+    </div>
+  </div>
+</nav>
+
+
+<h1 class="display-1"><Center>Inventario Médico </h1>
+<h2> ¿Cómo Descargar el Programa? </h2>
+<blockquote class="blockquote">
+  <p>Antes de iniciar a usar este programa, primero se debe conocer como descargarlo y hacer uso de todas las Funciones</p> 
+  <footer class="blockquote-footer">Se utilizará en especial una base de datos la cual si debemos descargar un aplicación previamente</footer> 
+</blockquote>
+<br>
+
+<div class="card mb-3" style="background-color: #F0FFF0 ">
+  <div class="card-body">
+    <h4 class="card-title">Se necestita el servidor WAMP</h4>
+    <p class="card-text"><h5>Wamp es un paquete para instalar un servidor web en sistemas operativos Windows. Permite instalar desde un único paquete diferentes servicios que son totalmente necesarios para montar una web en nuestro PC con sistema operativo Windows. Wamp incorpora: </h5></p>
+    <ol><h6>
+    <li>Apache: Es el servidor web HTTP y HTTPS de código abierto, es uno de los más utilizados en todo el mundo, y está disponible para su uso en plataformas Windows, Linux, Unix, MacOS y otros sistemas operativos como FreeBSD. Apache normalmente se utiliza para webs estáticas y también dinámicas </li><br>
+    <li>MySQL: este sistema de gestión de base de datos relacional es uno de los más utilizados en todo el mundo, se usa con aplicaciones web como WordPress, Drupal, phpBB y muchos otros más</li> <br>
+    <li>PHP: este lenguaje de programación interpretado se ha diseñado específicamente para webs dinámicas, pero también se puede utilizar para webs estáticas. </li>
+</h6></ol>
+<center><img src="https://www.redeszone.net/app/uploads-redeszone.net/2015/11/Wamp-Server-para-Windows.png" width="500px" >
+<p><h2>
+        <a href="https://sourceforge.net/projects/wampserver/files/"&gt; Enlace </a></h2>
+    </p>
+    <p><h4>WampServer Files - A Windows Web development environment for Apache, MySQL, PHP databases </h4>
+        <a href="https://sourceforge.net/projects/wampserver/files/"&gt; Enlace ></a> </h2>
+    </p>
+  </div>
+</div>
+<BR>
+<div class="card"  style="background-color: #F0FFF0 ">
+  <div class="card-body">
+    <h5 class="card-title">¿Cómo descargar el Programa?</h5>
+    <p class="card-text">El inventario se transmitirá mediante un archivo '.sln' lo cual puede llegar a necesitar un software especial, por ejemplo software Windows Installer XML Solution. Sin embargo, no es la única manera de realizarlo Si no tiene el software en el que se creó su archivo SLN en su PC, puede abrirlo estableciendo sus asociaciones de archivos predeterminadas para abrir archivos SLN en un programa similar, como como Asterisk Native Data Format, o Visual Studio .NET Solution, o cualquier otro software que use archivos SLN. <br>
+    <BR>
+    File Magic es un visor de software universal que puede abrir la mayoría de las extensiones de archivos, incluidos algunos archivos binarios SLN. Al descargar el Visor de software universal de File Magic, ahora puede abrir archivos incluso cuando no tenga el software. Le ayudará a ahorrar recursos funcionales en su PC, ya que no tiene tantos programas en ejecución y por lo tanto reduce los conflictos de software. Lo más importante es que no tendrá que gastar dinero en la compra de software costoso que solo puede usar una vez para ver su archivo SLN.</p>
+  </div>
+  <center><img src="https://docs.microsoft.com/es-es/visualstudio/ide/media/filtered-solutions/do-not-load-projects.png?view=vs-2017&viewFallbackFrom=vs-2022" width="500px" >
+</div>
+</body>
+</html>
