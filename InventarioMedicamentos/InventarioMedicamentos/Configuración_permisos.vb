3b61ca4125ec21675ef880b9a068f97b2ce39683
Mariella-Ixquiac <imixquiachernandez2005@elvallecolegio.edu.gt>
2022-09-26 11:30:16 -0600

    comentario

--- InventarioMedicamentos/InventarioMedicamentos/Configuración_permisos.vb
+++ InventarioMedicamentos/InventarioMedicamentos/Configuración_permisos.vb
@@ -25,6 +25,7 @@ Public Class Configuración_permisos
     Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
         Me.Hide()
         Index.Show()
+        ' hola :)
     End Sub
 
     Private Sub Configuración_permisos_Load(sender As Object, e As EventArgs) Handles MyBase.Load
